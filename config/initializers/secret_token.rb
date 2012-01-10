7e79eeb45d5c9b0c3df1eee02e1f0def462afacb
Bence Magyar <bence.magyar@gmail.com>
2012-01-09 23:23:26 -0500

    local commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fieldlink::Application.config.secret_token = '5447a5d2433ce83585b79e958d3ac8003857a762cb6de088179f9f3fa85f8be8829b5f4e8b5420306a195b63404815823150150e4c4e2d01c1180b458a64e2d5'
